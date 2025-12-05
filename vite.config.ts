551830c6dd72beae5fd9dc1ac2a3bf17aa6a09eb
moyan <1561515308@qq.com>
2025-12-05 21:39:04 +0800

    build(deps): 添加 vite-plugin-version-mark 依赖项

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,18 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { vitePluginVersionMark } from "vite-plugin-version-mark";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+
     vue(),
+    vitePluginVersionMark({
+      ifShortSHA: true
+    }),
     vueDevTools(),
+
   ],
   resolve: {
     alias: {
